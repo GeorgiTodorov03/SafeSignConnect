cc905972dad5ae19fb01bc6e07638c21380ade80
GeorgiTodorov03 <ge.todorov03@gmail.com>
2025-08-27 14:29:34 +0300

    docs: Added description to the interface.

--- src/main/java/Dao/UserDAO.java
+++ src/main/java/Dao/UserDAO.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public interface UserDAO {
 
+    // UserDAO(Data Access Object) this interface, and it's implementation are created to
+    // separate data persistence logic(database operations) from the business logic(services, controllers)
+
     void save(User user);
     User findById(Long id);
     List<User> findAll();
